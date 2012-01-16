1c8d53babdf2f54e862b8b26149aa6a2e1f284d7
Aaron Addleman <aaron@9thport.net>
2012-01-15 21:10:15 -0800

    added default files of rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinthportNet::Application.config.secret_token = 'adf7a8819715c042a179a1c585a6c00915c55c3b4ea0c15864664422d4cb72fc9504384e0c3798486b09815982594d557daba0a959dbc310e9e9d0b6bfa84b9e'
