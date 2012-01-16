c97b7cdf3cd8cce3aabd6d45f8d3307e5bbf1f64
Aaron Addleman <aaron@9thport.net>
2012-01-15 20:43:24 -0800

    removed rails 2.x files

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '7b5f9fa3284c2a07faa6933907d2ede5a0e0354b5b9f9e129f344acc9d503f4d257f05880f82587e404064318aab1d70c8cd3816fe9effdf2a72e08216a5bea8';
